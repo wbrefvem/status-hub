4e48e8832b9a013cdb300de945c66b27d1df0327
Will Refvem <wbrefvem@gmail.com>
2015-06-24 16:50:54 -0400

    More celery changes

---
+++ status_hub/celery.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+
+import os
+
+from celery import Celery
+
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'status_hub.settings')
+
+app = Celery('status_hub')
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+
+
+@app.task(bind=True)
+def add(x, y):
+    print "You're soooooooo good looking"
+    return x + y
