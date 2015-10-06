88cd468a6f5963482fa5e8ac920f9221e8689e47
Will Refvem <wbrefvem@gmail.com>
2015-10-06 14:16:06 -0400

    Updates to Travis config, secrets

--- status_hub/settings.py
+++ status_hub/settings.py
@@ -89,12 +89,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'status_hub.wsgi.application'
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'statushub',
+    #     'USER': 'postgres',
+    #     'HOST': 'localhost',
+    #     'PORT': 5432
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'statushub',
-        'USER': 'postgres',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
